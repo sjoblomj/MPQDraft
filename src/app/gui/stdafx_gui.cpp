c31609049ab6ea15f5fae1f0f51a1d0fc97459bf
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-12-04 19:51:09 +0100

    Removing legacy MFC GUI; moving Qt GUI in its place

--- src/app/gui/stdafx_gui.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx_gui.cpp : source file that includes just the standard includes
-//	MPQDraft.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx_gui.h"
-
